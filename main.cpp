51a0973e85ff21855a2949b94122ecd72ceb7f0c
Kevin Allen <kma1660@gmail.com>
2016-01-30 12:13:31 -0500

    Fixed indention

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 using namespace cv;
 int main ( int argc, char **argv )
 {
-  Mat frame = imread("/home/mike/Downloads/half-life-2-episode-one-2d.jpg", 1 );
-  //VideoCapture cap(0);
-  namedWindow("Display Image", WINDOW_NORMAL );
-  imshow("Display Image", frame);
-  waitKey(0);
-  return 0;
+	  Mat frame;
+	  VideoCapture cap(1);
+	  namedWindow("Display Image", WINDOW_NORMAL );
+	  cap >> frame;
+	  imshow("Display Image", frame);
+	  waitKey(0);
+	  return 0;
 }
