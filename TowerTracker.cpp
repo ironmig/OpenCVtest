d1fc196b4cf511d35132043f5a97f4579eca8121
Kevin Allen <kma1660@gmail.com>
2016-01-30 21:29:05 -0500

    Completely refactored into a TowerTracker class, compiler var for GUI

---
+++ TowerTracker.cpp
@@ -0,0 +1,109 @@
+#include "TowerTracker.h"
+
+TowerTracker::TowerTracker() {
+	frame = Mat(height,width,CV_8UC3); //inital frame
+	blur_frame = Mat(height,width,CV_8UC3); //blured frame
+	hsv_frame = Mat(height,width,CV_8UC3); //converted to HSV frame
+	binary_frame = Mat(height,width,CV_8UC1);
+	erode_element = getStructuringElement(MORPH_RECT,Size(erode_kernel_size,erode_kernel_size), Point(-1,1));
+	dilate_element = getStructuringElement(MORPH_RECT,Size(dilate_kernel_size,dilate_kernel_size),Point(-1,1));
+	contours = std::vector < std::vector <Point> >();
+	rectangles = std::vector < RotatedRect >();
+	cap = cv::VideoCapture(1);
+}
+void TowerTracker::ThresholdFrame(cv::Mat& src, cv::Mat& dest, TowerTracker::ThresholdValues thresh)
+{
+	inRange(src,cv::Scalar(thresh.HLOW,thresh.SLOW,thresh.VLOW),Scalar(thresh.HHIGH,thresh.SHIGH,thresh.VHIGH),dest);
+}
+void TowerTracker::ErodeFrame(cv::Mat& src, cv::Mat& dest, cv::Mat erode_src)
+{
+	erode(src,dest, erode_src);
+}
+void TowerTracker::DilateFrame(cv::Mat& src, cv::Mat& dest, cv::Mat dilate_src)
+{
+	dilate(src,dest,dilate_src);
+}
+bool TowerTracker::RectangleSorter(RotatedRect x, RotatedRect y)
+{
+	return x.size.area() > y.size.area();
+}
+void TowerTracker::GetRectangles(std::vector < std::vector <Point> > contours,std::vector<RotatedRect> rectangles)
+{
+	  for (std::vector<Point > cur : contours)
+	  {
+		  rectangles.push_back(minAreaRect (cur));
+	  }
+}
+void TowerTracker::GetContours (InputOutputArray image, OutputArrayOfArrays contours)
+{
+	findContours(image, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
+}
+void TowerTracker::ConvertToHSV (Mat& src, Mat& dest)
+{
+	cvtColor(src,dest,CV_BGR2HSV);
+}
+void TowerTracker::BlurFrame(Mat& src, Mat& dest, int kernel_size)
+{
+	medianBlur(src,dest,kernel_size);
+}
+void TowerTracker::run()
+{
+	#if DEBUG_GUI
+	namedWindow(mainWindow, WINDOW_AUTOSIZE );
+	namedWindow(altWindow,WINDOW_NORMAL);
+	#endif
+
+	if (!cap.isOpened())
+	{
+		std::cout << "Camera opening error" << std::endl;
+		return;
+	}
+	cap.set(CAP_PROP_FRAME_WIDTH,width);
+	cap.set(CAP_PROP_FRAME_HEIGHT, height);
+	cap.set(CAP_PROP_FPS, frameRate);
+	cap.set(CAP_PROP_BRIGHTNESS,brightness);
+
+	int key;
+	while (true)
+	{
+		key = waitKey(200) & 255;
+		if (key == 27)break;
+
+		contours.clear();
+		rectangles.clear();
+
+		if (!cap.read(frame))
+		{
+			std::cout << "Could not read frame" << std::endl;
+			continue;
+		}
+
+		BlurFrame(frame,blur_frame,blur_kernel_size);
+		ConvertToHSV (blur_frame,hsv_frame);
+		ThresholdFrame(hsv_frame,binary_frame,thresh);
+		ErodeFrame(binary_frame,binary_frame, erode_element);
+		DilateFrame(binary_frame,binary_frame, dilate_element);
+		GetContours (binary_frame, contours);
+		GetRectangles(contours,rectangles);
+		std::sort(rectangles.begin(),rectangles.end(),RectangleSorter);
+
+
+		#if DEBUG_GUI
+		//Debugging GUI Stuff
+		if (rectangles.size() >= 1)
+		{
+			Point2f pts[4];
+			rectangles.at(0).points(pts);
+			rectangle(frame,pts[0],pts[2],Scalar(0,0,0));
+		}
+
+		drawContours(frame,contours,-1,Scalar(0,255,0),3);
+		imshow(mainWindow, binary_frame);
+		imshow(altWindow,frame);
+		#endif
+	}
+}
+TowerTracker::~TowerTracker()
+{
+
+}
---
+++ TowerTracker.h
@@ -0,0 +1,59 @@
+#ifndef SRC_TOWERTRACKER_H_
+#define SRC_TOWERTRACKER_H_
+
+//OpenCV test
+#include "opencv2/opencv.hpp"
+#include  <algorithm>
+
+#define DEBUG_GUI true
+
+using namespace cv;
+class TowerTracker {
+private:
+	struct ThresholdValues {
+		double HLOW;
+		double HHIGH;
+		double SLOW;
+		double SHIGH;
+		double VLOW;
+		double VHIGH;
+	};
+	static void ThresholdFrame(cv::Mat& src, cv::Mat& dest, TowerTracker::ThresholdValues thresh);
+	static void ErodeFrame(cv::Mat& src, cv::Mat& dest, cv::Mat erode_src);
+	static void DilateFrame(cv::Mat& src, cv::Mat& dest, cv::Mat dilate_src);
+	static bool RectangleSorter(RotatedRect x, RotatedRect y);
+	static void GetContours (InputOutputArray image, OutputArrayOfArrays contours);
+	static void ConvertToHSV(Mat& src, Mat& dest);
+	static void BlurFrame(Mat& src, Mat& dest, int kernel_size);
+	static void GetRectangles(std::vector < std::vector <Point> > contours, std::vector<RotatedRect> rectangles);
+
+	const int height = 640;
+	const int width = 480;
+	const int frameRate = 30;
+	const double brightness = 115.0/255;
+	const int blur_kernel_size = 3;
+	const int erode_kernel_size = 3;
+	const int dilate_kernel_size = 3;
+	const struct ThresholdValues thresh = {40,80,80,130,115,255};
+
+	cv::VideoCapture cap;
+	Mat frame;
+	Mat blur_frame;
+	Mat hsv_frame;
+	Mat binary_frame;
+	Mat erode_element;
+	Mat dilate_element;
+	std::vector < std::vector <Point> > contours;
+	std::vector < RotatedRect > rectangles;
+
+	#if DEBUG_GUI
+	const std::string mainWindow = "main";
+	const std::string altWindow = "alt";
+	#endif
+public:
+	TowerTracker();
+	void run();
+	virtual ~TowerTracker();
+};
+
+#endif /* SRC_TRACKTOWER_H_ */
